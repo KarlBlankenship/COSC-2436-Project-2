e1de7b6f599f9b2ed1f0458109eaeb21624bb4f4
Karl Blankenship <Karleb777@gmail.com>
2018-12-08 10:23:03 -0600

    Created the employee class to hold employee name and salary.

--- src/Project2/BinarySearchTree.java
+++ src/Project2/BinarySearchTree.java
@@ -9,8 +9,75 @@ package Project2;
  *
  * @author Karl
  */
-public class BinarySearchTree {
+public class BinarySearchTree 
+{
 
+    private class Employee
+    {
+        // Create private variables to hold employee name and employee salary.
+        private String name;
+        private int salary;
+    
+        /**
+         * Default constructor.
+         */
+        public Employee() 
+        {
+        }
+
+        /**
+         * Overload constructor.
+         * @param name
+         * @param salary 
+         */
+        public Employee(String name, int salary) 
+        {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        /**
+         * Accessor method for getting employee name.
+         * @return name
+         */
+        public String getName() 
+        {
+            return name;
+        }
+    
+        /**
+         * Mutator method for setting employee name.
+         * @param name 
+         */
+        public void setName(String name) 
+        {
+            this.name = name;
+        }
+    
+        /**
+         * Accessor method for getting employee salary.
+         * @return salary
+         */
+        public int getSalary() 
+        {
+            return salary;
+        }
+
+        /**
+         * Mutator method for setting employee salary.
+         * @param salary
+         */
+        public void setManager(int salary) 
+        {
+            this.salary = salary;
+        }        
+    }
+    
+    private class Node
+    {
+        
+    }
+    
     /**
      * @param args the command line arguments
      */
